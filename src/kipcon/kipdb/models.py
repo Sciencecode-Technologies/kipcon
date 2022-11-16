598ad9b3077aba32fb5a64eaf295207033c8a353
programmer-666 <suharslan@protonmail.ch>
2022-11-16 14:10:52 +0300

    update

--- src/kipcon/kipdb/models.py
+++ src/kipcon/kipdb/models.py
@@ -3,4 +3,17 @@ from sqlalchemy import MetaData, Table, Column, Integer, String
 
 class KipTables:
     def __init__(self):
+        self.table_names: list = [
+            "clients",
+            "conf_types",
+            "config_files",
+            "transactions"
+        ]
+
+        self.consts: list = [
+            "CONST_FOLDERS",
+            "CONST_TOKENS"
+        ]
+
+    def CONSTS(self):
         pass
