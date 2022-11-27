687b357b5c609e4ac1aed29adc9280e1cb5b74b9
programmer-666 <suharslan@protonmail.ch>
2022-11-27 23:56:49 +0300

    update

--- tests/db_tests/db_transactions.py
+++ tests/db_tests/db_transactions.py
@@ -3,4 +3,4 @@ from kipcon.kipdb import *
 
 if __name__ == "__main__":
     const_folders_transactions = Database_Transactions.const_folders()
-    const_folders_transactions.main_folder_name.change()
+    const_folders_transactions.main_folder_name
