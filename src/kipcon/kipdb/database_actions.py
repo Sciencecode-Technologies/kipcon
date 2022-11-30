d85d01f0fa4b53cc5006592aed86e0f1fc70bbb6
programmer-666 <suharslan@protonmail.ch>
2022-12-01 00:41:59 +0300

    db_actions update (practice remove)

--- src/kipcon/kipdb/database_actions.py
+++ src/kipcon/kipdb/database_actions.py
@@ -2,29 +2,6 @@ from kipcon.kipdb.models import CONST_FOLDERS
 from sqlalchemy import update
 
 
-"""def add_client(func):
-    def inner(a):
-        return func(a)
-    return inner
-
-def add_client2(func):
-    def inner():
-        return func(3)
-    return inner
-
-def add_client3(*args, **kwargs):
-    def inner(func):
-        print(kwargs['f'])
-        func()
-    return inner
-
-def addx(**kwargs):
-    def inner(func):
-        return func(kwargs['x']*2)
-    return inner"""
-# PRACTICE
-
-
 def const_folders(**kwargs):
     def inner(func):
         new_value = func()
--- tests/db_tests/db_transactions.py
+++ tests/db_tests/db_transactions.py
@@ -2,27 +2,6 @@ from kipcon.kipdb.database_actions import *
 
 
 if __name__ == "__main__":
-    """@add_client
-    def test(x):
-        return x
-
-    @add_client2
-    def test2(x):
-        return x
-
-    @add_client3(f = 52)
-    def test3():
-        print(81)
-    
-    print(test(2))
-    print(test2()) # 3
-
-    @addx(x = 200)
-    def calc(y): # xy
-        return y    
-
-    print(calc)"""
-    # PRACTICE
 
     @const_folders(column = "main_folder_name")
     def update(value = "main2"):
